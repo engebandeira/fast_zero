4e0d117ad46d488ac34ac08534bdea77dff95f82
animasouza <animasouza@gmail.com>
2025-06-20 10:45:36 -0300

    aula 02 - fundamentos de desenvolvimento web

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,29 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
-app = FastAPI()
+from fast_zero.schemas import Message
+
+app = FastAPI(title='Minha API')
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Hello World'}
+
+
+@app.get(
+    '/exercicio-02', status_code=HTTPStatus.OK, response_class=HTMLResponse
+)
+def exercicio_aula_02():
+    return """
+           <html>
+               <head>
+                   <title>Nosso Olá mundo</title>
+               </head>
+               <body>
+                   <h1> Olá Mundo! </h1>
+               </body>
+           </html>
+           """
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
+
 from fastapi.testclient import TestClient
+
 from fast_zero.app import app
 
 
@@ -10,3 +12,12 @@ def test_root_deve_retornar_ok_e_ola_mundo():
 
     assert response.status_code == HTTPStatus.OK  # Assert (afirmar)
     assert response.json() == {'message': 'Hello World'}
+
+
+def test_exercicio_ola_mundo_em_html():
+    client = TestClient(app)
+
+    response = client.get('/exercicio-02')
+
+    assert response.status_code == HTTPStatus.OK
+    assert '<h1> Olá Mundo! </h1>' in response.text
