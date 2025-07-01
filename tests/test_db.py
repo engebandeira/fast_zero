1147f3e3fc06ebae9700abb39c70939250561d10
animasouza <animasouza@gmail.com>
2025-07-01 08:52:15 -0300

    Aula 04 - Parcial

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,9 +1,25 @@
+from dataclasses import asdict
+
+from sqlalchemy import select
+
 from fast_zero.models import User
 
 
-def test_create_user(session):
-    user = User(username='test', email='test@test.com', password='secret')
-    session.add(user)
+def test_create_user(session, mock_db_time):
+    with mock_db_time(User) as time:
+        new_user = User(
+            username='test', email='test@test.com', password='secret'
+        )
 
-    assert user.username == 'test'
-    assert user.password == 'secret'
+        session.add(new_user)
+        session.commit()
+
+        user = session.scalar(select(User).where(User.username == 'test'))
+
+    assert asdict(user) == {
+        'id': 1,
+        'username': 'test',
+        'email': 'test@test.com',
+        'password': 'secret',
+        'created_at': time,
+    }
